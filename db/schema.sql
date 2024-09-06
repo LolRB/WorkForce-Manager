70e2d02c3eb948f90654dc3db740daa3a5fee01d
LolRB <rodrigo_luuu@hotmail.com>
2024-09-06 03:21:12 -0600

    Created schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS company;
+
+CREATE DATABASE company;
+
+\connect company
+
+CREATE TABLE departments (
+  departments_id SERIAL PRIMARY KEY,
+  departments_name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE roles (
+  roles_id SERIAL PRIMARY KEY,
+  roles_title VARCHAR(50) NOT NULL UNIQUE,
+  roles_salary NUMERIC NOT NULL,
+  dept_id INTEGER NOT NULL,
+  CONSTRAINT fk_dept FOREIGN KEY (dept_id) REFERENCES departments(departments_id) ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+  employees_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  position_id INTEGER NOT NULL,
+  manager_ref INTEGER,
+  CONSTRAINT fk_position FOREIGN KEY (position_id) REFERENCES roles(roles_id) ON DELETE CASCADE,
+  CONSTRAINT fk_manager_ref FOREIGN KEY (manager_ref) REFERENCES employees(employees_id) ON DELETE SET NULL
+);
